d42169ab073c3d0c07b6f7a427729bac18ff1ff3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 11:31:40 +0100

    fix: refactor

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,67 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import { StatusCodes } from 'http-status-codes';
+
+const protect = asyncMiddleware(async (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    token = authHeader.split(' ')[1];
+  }
+
+  // token verification
+  if (!token) {
+    return next(
+      new UnauthenticatedError(
+        'You are not logged in! Please log in to get access'
+      )
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET_KEY
+  );
+
+  // check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new UnauthenticatedError(
+        'The user belonging to this token does no longer exist'
+      )
+    );
+  }
+
+  // check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new UnauthenticatedError(
+        'User recently changed password! Please log in again'
+      )
+    );
+  }
+
+  // grant access to protected routes
+  req.user = currentUser;
+  next();
+});
+
+const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ForbiddenError('You do not have permission to perform this action')
+      );
+    }
+    next();
+  };
+};
+
+const authMiddleware = {
+  protect,
+  restrictTo,
+};
+
+export default authMiddleware;
