8c3c0f7b078d73a595e8ab592be44689d7c886f0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 09:50:22 +0100

    fix: refactor mongodb connection to a file

---
+++ config/db.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config({ path: './config.env' });
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { DATABASE, DATABASE_PASSWORD, DATABASE_LOCAL } = process.env;
+
+const dbLocal = DATABASE_LOCAL;
+const mongoURI = DATABASE.replace('<PASSWORD>', DATABASE_PASSWORD);
+
+const db = devEnv ? dbLocal : mongoURI;
+
+const connectDB = async () => {
+  try {
+    const conn = mongoose.connect(db);
+    console.log(`Connected to MongoDB successfully → ${conn.connection.port}`.gray.bold);
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default connectDB;
