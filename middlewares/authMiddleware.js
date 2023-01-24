825eb1d7ea0ef8f0d8ea2d51a07e47c613371b1a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 11:45:13 +0100

    fix: modify

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -46,16 +46,16 @@ const protect = asyncMiddleware(async (req, res, next) => {
   next();
 });
 
-const restrictTo = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new ForbiddenError('You do not have permission to perform this action')
-      );
-    }
-    next();
-  };
-};
+const restrictTo =
+  (...roles) =>
+    (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        return next(
+          new ForbiddenError('You do not have permission to perform this action')
+        );
+      }
+      next();
+    };
 
 const verifyUser = (req, res, next) => {
   if (
