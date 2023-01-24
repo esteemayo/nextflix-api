375625cd0f6bc60718af193e20aea90956ad9936
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 09:50:00 +0100

    fix: refactor

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-require('colors');
+import 'colors';
 
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...'.red.bold);
@@ -8,30 +6,15 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: './config.env' });
 import app from './app.js';
-
-// db local
-const dbLocal = process.env.DATABASE_LOCAL;
-
-// atlas mongo uri
-const mongoURI = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
-// MongoDB connection
-mongoose
-  .connect(dbLocal)
-  .then(() =>
-    console.log(`Connected to MongoDB successfully → ${dbLocal}`.gray.bold)
-  );
+import connectDB from './config/db.js';
 
 app.set('port', process.env.PORT || 9900);
 
-const server = app.listen(app.get('port'), () =>
+const server = app.listen(app.get('port'), async () => {
+  await connectDB();
   console.log(`Server running on port ${server.address().port}`.blue.bold)
-);
+});
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 🔥 Shutting down...'.red.bold);
