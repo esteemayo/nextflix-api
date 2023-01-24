b3334a013b18ad90470bc8ee8d88d9726b27028a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 13:27:58 +0100

    fix: refactor

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -1,18 +1,15 @@
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = user.generateAuthToken();
 
-  const cookieOptions = {
+  res.cookie('jwtToken', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     sameSite: true,
     signed: true,
-  };
-
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwtToken', token, cookieOptions);
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
 
   const { password, role, ...rest } = user._doc;
 
