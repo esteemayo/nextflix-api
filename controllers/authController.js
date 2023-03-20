db9455a3a4aaf688ae53a8d7289a809c31cc1d1c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-20 16:24:20 +0100

    fix: modify

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,7 @@ import BadRequestError from '../errors/badRequest.js';
 import asyncMiddleware from '../utils/asyncMiddleware.js';
 import CustomAPIError from '../errors/customAPIError.js';
 import createSendToken from '../utils/createSendToken.js';
+import CustomAPIError from '../errors/customAPIError.js';
 import UnauthenticatedError from '../errors/unauthenticated.js';
 
 const login = asyncMiddleware(async (req, res, next) => {
