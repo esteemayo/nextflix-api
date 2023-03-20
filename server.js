a48e52c23a039f19bebc57061f4883fce43a27cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-20 15:48:22 +0100

    fix: modify

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+import app from './app.js';
+import connectDB from './config/db.js';
+
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...'.red.bold);
   console.log(err.name, err.message);
   process.exit(1);
 });
-
-import app from './app.js';
-import connectDB from './config/db.js';
 
 app.set('port', process.env.PORT || 9900);
 
