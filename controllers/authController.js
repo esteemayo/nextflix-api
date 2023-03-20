ee9fd802263952aa68e8948ce5ae57794e2d016f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-20 16:24:29 +0100

    fix: modify

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,7 @@ import NotFoundError from '../errors/notFound.js';
 import sendEmail from '../utils/email.js';
 import BadRequestError from '../errors/badRequest.js';
 import asyncMiddleware from '../utils/asyncMiddleware.js';
-import CustomAPIError from '../errors/customAPIError.js';
+import BadRequestError from '../errors/badRequest.js';
 import createSendToken from '../utils/createSendToken.js';
 import CustomAPIError from '../errors/customAPIError.js';
 import UnauthenticatedError from '../errors/unauthenticated.js';
